931526b8824e9cc66d908f957f0d7d4a0d940785
mdrisk <mrisk@westclarkschools.com>
2018-02-15 10:51:08 -0500

    work commit 2-15

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import reducers from './reducers';
+
+import PostIndex from './components/post_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('.container')
+);
